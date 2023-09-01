a7826142e8f6217c3c2a1785c53074112e1f18cc
Sarvesh Jagtap <sarveshvjagtap@gmail.com>
2023-09-01 17:37:29 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EduBoost',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
